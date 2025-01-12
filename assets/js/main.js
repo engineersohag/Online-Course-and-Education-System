5d8c727af9bc21c0aad7f07e6a793b6772bb95ef
engineersohag <engineer.sohag.cse@gmail.com>
2025-01-12 19:45:40 +0600

    School Management System

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+
+// Home-hover-start
+$('#hoverImg').hover(function(){
+	$('#d-none-p').toggle('slow');
+});
+
+$('#hoverImg-1').hover(function(){
+	$('#d-none-p-1').toggle('slow');
+});
+
+$('#hoverImg-2').hover(function(){
+	$('#d-none-p-2').toggle('slow');
+});
+
+$('#hoverImg-3').hover(function(){
+	$('#d-none-p-3').toggle('slow');
+});
+
+$('#hoverImg-4').hover(function(){
+	$('#d-none-p-4').toggle('slow');
+});
+
+$('#hoverImg-5').hover(function(){
+	$('#d-none-p-5').toggle('slow');
+});
+
+$('#hoverImg-6').hover(function(){
+	$('#d-none-p-6').toggle('slow');
+});
+
+$('#hoverImg-7').hover(function(){
+	$('#d-none-p-7').toggle('slow');
+});
+$('#hoverImg-8').hover(function(){
+	$('#d-none-p-8').toggle('slow');
+});
+
+$('#hoverImg-9').hover(function(){
+	$('#d-none-p-9').toggle('slow');
+});
+
+$('#hoverImg-10').hover(function(){
+	$('#d-none-p-10').toggle('slow');
+});
+
+$('#hoverImg-11').hover(function(){
+	$('#d-none-p-11').toggle('slow');
+});
+$('#hoverImg-12').hover(function(){
+	$('#d-none-p-12').toggle('slow');
+});
+
+$('#hoverImg-13').hover(function(){
+	$('#d-none-p-13').toggle('slow');
+});
+$('#hoverImg-14').hover(function(){
+	$('#d-none-p-14').toggle('slow');
+});
+
+$('#hoverImg-15').hover(function(){
+	$('#d-none-p-15').toggle('slow');
+});
+
+$('#hoverImg-16').hover(function(){
+	$('#d-none-p-16').toggle('slow');
+});
+
+$('#hoverImg-17').hover(function(){
+	$('#d-none-p-17').toggle('slow');
+});
+
+
+//Deshboart hover
+
+$('#desh').click(function(){
+	$('#desh-centent').css({
+		"margin-left":"200px",
+		"margin-top" :"-20px"
+	}).toggle('slow');
+});
+
+$('#desh-2').click(function(){
+	$('#desh-centent-2').css("margin-left","160px").toggle('slow');
+});
